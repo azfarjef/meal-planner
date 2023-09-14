c6489cf4607911edfbb49e6ff83a31670ee97c06
azfarjef <muhammadazfarjef@gmail.com>
2023-09-14 14:05:14 +0800

    Add date range picker for user to generate grocery checklist for preferred dates

--- client/src/scene/grocery/index.tsx
+++ client/src/scene/grocery/index.tsx
@@ -1,21 +1,21 @@
-// import { Typography } from "@mui/material";
-
 import { useGetGroceryListQuery } from "@/state/api";
 import {
   Avatar,
-  Button,
+  Box,
   Checkbox,
   Container,
   FormControlLabel,
-  Grid,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
-  TextField,
   Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
+
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import daysjs, { Dayjs } from "dayjs";
 
 interface GroceryItem {
   fdc_id: number;
@@ -57,42 +57,15 @@ interface GroceryItem {
 //   },
 // ];
 
-// const Grocery = () => {
-//   return (
-//     <div>
-//       <h1>Grocery</h1>
-//       <Typography variant="h4">
-//         Total : RM{" "}
-//         {groceries.reduce((acc, curr) => acc + curr.price, 0).toFixed(2)}
-//       </Typography>
-//       <Typography>
-//         {groceries.map((grocery) => (
-//           <div>
-//             <img
-//               style={{ width: "100px" }}
-//               src={grocery.image}
-//               alt={grocery.name}
-//             />
-//             <h3>{grocery.name}</h3>
-//             <p>{grocery.amount}</p>
-//             <p>{grocery.unit}</p>
-//             <p>{grocery.price}</p>
-//           </div>
-//         ))}
-//       </Typography>
-//     </div>
-//   );
-// };
-
-// export default Grocery;
-
 const Grocery = () => {
+  const [dateFrom, setDateFrom] = useState<Dayjs>(daysjs());
+  const [dateTo, setDateTo] = useState<Dayjs>(daysjs().add(7, "day"));
+
   const { data, isLoading } = useGetGroceryListQuery({
-    dateFrom: "2023-08-19",
-    dateTo: "2023-08-29",
+    dateFrom: dateFrom.format("YYYY-MM-DD"),
+    dateTo: dateTo.format("YYYY-MM-DD"),
   });
 
-  // const [item, setItem] = useState("");
   const [items, setItems] = useState<GroceryItem[]>(data?.groceries || []);
 
   useEffect(() => {
@@ -101,87 +74,58 @@ const Grocery = () => {
     }
   }, [data]);
 
-  // useEffect(() => {
-  //   const storedItems = localStorage.getItem("groceryItems");
-  //   if (storedItems) {
-  //     setItems(JSON.parse(storedItems));
-  //   }
-  // }, []);
-
-  // // Load item.bought state from localStorage on component mount
-  // useEffect(() => {
-  //   const storedItems = JSON.parse(
-  //     localStorage.getItem("groceryItems") || "[]"
-  //   );
-  //   setItems(storedItems);
-  // }, []);
-
-  // // Save item.bought state to localStorage whenever items change
-  // useEffect(() => {
-  //   localStorage.setItem("groceryItems", JSON.stringify(items));
-  // }, [items]);
-
   if (isLoading || !data) {
     return <div>Loading...</div>; // Add a loading state or spinner while data is being fetched
   }
-
-  // const { groceries, total_price } = data;
-
-  // const addItem = () => {
-  //   if (item.trim() !== "") {
-  //     setItems([
-  //       ...items,
-  //       {
-  //         fdc_id: 0,
-  //         name: item,
-  //         amount: 1,
-  //         // unit: "count",
-  //         price: 0,
-  //         image: "",
-  //         bought: false,
-  //       },
-  //     ]);
-  //     setItem("");
-  //   }
-  // };
 
   const toggleItemBought = (index: number) => {
     const updatedItems = items.map((item, i) =>
       i === index ? { ...item, bought: !item.bought } : item
     );
     setItems(updatedItems);
-
-    // // Store the updated items in localStorage
-    // localStorage.setItem("groceryItems", JSON.stringify(updatedItems));
   };
 
   return (
     <div>
       <Container>
-        <h2>Grocery Checklist</h2>
-        <h2>Total Price: RM {data.total_price}</h2>
+        <Typography variant="h4" gutterBottom>
+          Grocery Checklist
+        </Typography>
 
-        {/* <Grid container spacing={2} alignItems="center">
-          <Grid item xs={12} sm={6} md={4}>
-            <TextField
-              fullWidth
-              label="Item"
-              variant="outlined"
-              value={item}
-              onChange={(e) => setItem(e.target.value)}
+        <Typography variant="h6" gutterBottom>
+          Select a date range to generate a grocery list
+        </Typography>
+        <Box my={4}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Start"
+              maxDate={dateTo}
+              sx={{ minWidth: "230px", mb: 2 }}
+              format="dddd, DD/MM/YYYY"
+              value={dateFrom}
+              onChange={(newDateFrom) =>
+                newDateFrom ? setDateFrom(newDateFrom) : null
+              }
             />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <Button
-              fullWidth
-              variant="contained"
-              color="primary"
-              onClick={addItem}
-            >
-              Add Item
-            </Button>
-          </Grid>
-        </Grid> */}
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="End"
+              minDate={dateFrom}
+              sx={{ minWidth: "230px", mb: 2 }}
+              format="dddd, DD/MM/YYYY"
+              value={dateTo}
+              onChange={(newDateTo) =>
+                newDateTo ? setDateTo(newDateTo) : null
+              }
+            />
+          </LocalizationProvider>
+        </Box>
+
+        <Typography variant="h5">
+          Total Price: RM {data.total_price.toFixed(2)}
+        </Typography>
+
         <List>
           {items.map((item, index) => (
             <ListItem key={index}>
@@ -225,10 +169,11 @@ export default Grocery;
 
 // const Grocery = () => {
 //   const { data, isLoading } = useGetGroceryListQuery({
-//     dateFrom: "2023-08-11",
-//     dateTo: "2023-08-12",
+//     dateFrom: "2023-08-19",
+//     dateTo: "2023-08-29",
 //   });
 
+//   // const [item, setItem] = useState("");
 //   const [items, setItems] = useState<GroceryItem[]>(data?.groceries || []);
 
 //   useEffect(() => {
@@ -237,23 +182,99 @@ export default Grocery;
 //     }
 //   }, [data]);
 
+//   // useEffect(() => {
+//   //   const storedItems = localStorage.getItem("groceryItems");
+//   //   if (storedItems) {
+//   //     setItems(JSON.parse(storedItems));
+//   //   }
+//   // }, []);
+
+//   // // Load item.bought state from localStorage on component mount
+//   // useEffect(() => {
+//   //   const storedItems = JSON.parse(
+//   //     localStorage.getItem("groceryItems") || "[]"
+//   //   );
+//   //   setItems(storedItems);
+//   // }, []);
+
+//   // // Save item.bought state to localStorage whenever items change
+//   // useEffect(() => {
+//   //   localStorage.setItem("groceryItems", JSON.stringify(items));
+//   // }, [items]);
+
 //   if (isLoading || !data) {
-//     return <div>Loading...</div>;
+//     return <div>Loading...</div>; // Add a loading state or spinner while data is being fetched
 //   }
+
+//   // const { groceries, total_price } = data;
+
+//   // const addItem = () => {
+//   //   if (item.trim() !== "") {
+//   //     setItems([
+//   //       ...items,
+//   //       {
+//   //         fdc_id: 0,
+//   //         name: item,
+//   //         amount: 1,
+//   //         // unit: "count",
+//   //         price: 0,
+//   //         image: "",
+//   //         bought: false,
+//   //       },
+//   //     ]);
+//   //     setItem("");
+//   //   }
+//   // };
 
 //   const toggleItemBought = (index: number) => {
 //     const updatedItems = items.map((item, i) =>
 //       i === index ? { ...item, bought: !item.bought } : item
 //     );
 //     setItems(updatedItems);
+
+//     // // Store the updated items in localStorage
+//     // localStorage.setItem("groceryItems", JSON.stringify(updatedItems));
 //   };
 
 //   return (
 //     <div>
 //       <Container>
 //         <h2>Grocery Checklist</h2>
+
+//         <Box>
+//           <LocalizationProvider dateAdapter={AdapterDayjs}>
+//             <DatePicker
+//               sx={{ minWidth: "230px", mb: 2 }}
+//               format="dddd, DD/MM/YYYY"
+//               value={date}
+//               onChange={handleSelectDate}
+//             />
+//           </LocalizationProvider>
+//         </Box>
+
 //         <h2>Total Price: RM {data.total_price}</h2>
 
+//         {/* <Grid container spacing={2} alignItems="center">
+//           <Grid item xs={12} sm={6} md={4}>
+//             <TextField
+//               fullWidth
+//               label="Item"
+//               variant="outlined"
+//               value={item}
+//               onChange={(e) => setItem(e.target.value)}
+//             />
+//           </Grid>
+//           <Grid item xs={12} sm={6} md={4}>
+//             <Button
+//               fullWidth
+//               variant="contained"
+//               color="primary"
+//               onClick={addItem}
+//             >
+//               Add Item
+//             </Button>
+//           </Grid>
+//         </Grid> */}
 //         <List>
 //           {items.map((item, index) => (
 //             <ListItem key={index}>
